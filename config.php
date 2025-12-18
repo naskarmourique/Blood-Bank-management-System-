29c4b33924c8c2dbf897e85d252edd8c89825911
Debangshu Roy <debangshuroy@gmail.com>
2025-12-18 22:01:02 +0530

    feat: Add email notification for contact form

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Email configuration
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USERNAME', 'bloodconnect8@gmail.com');
+define('SMTP_PASSWORD', 'uaga mivb vvwj qvwg');
+define('SMTP_PORT', 587);
+define('SMTP_SECURE', 'tls');
+
+// Admin Email
+define('ADMIN_EMAIL', 'naskarmourique@gmail.com');
+define('ADMIN_NAME', 'BloodConnect Admin');
+?>
--- insert_contact.php
+++ insert_contact.php
@@ -2,7 +2,15 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
 include "connect.php";
+require "config.php";
 
 if (isset($_POST["submit"])) {
     $f_name = $_POST["fName"] ?? null;
@@ -23,7 +31,35 @@ if (isset($_POST["submit"])) {
         $stmt->bind_param("ssssss", $f_name, $l_name, $email, $phone, $subject, $message);
 
         if ($stmt->execute()) {
-            header("Location: landing_page.php?status=success&message=" . urlencode('Your message has been sent successfully!'));
+            // Send confirmation email to user
+            $mail = new PHPMailer(true);
+            try {
+                //Server settings
+                $mail->isSMTP();
+                $mail->Host       = SMTP_HOST;
+                $mail->SMTPAuth   = true;
+                $mail->Username   = SMTP_USERNAME;
+                $mail->Password   = SMTP_PASSWORD;
+                $mail->SMTPSecure = SMTP_SECURE;
+                $mail->Port       = SMTP_PORT;
+
+                //Recipients
+                $mail->setFrom(SMTP_USERNAME, 'BloodConnect');
+                $mail->addAddress($email, $f_name . ' ' . $l_name);     //Add a recipient
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Thank you for contacting BloodConnect';
+                $mail->Body    = "Dear $f_name,<br><br>Thank you for contacting us. We have received your message and will get back to you shortly.<br><br><b>Your Message Summary:</b><br>Subject: $subject<br>Message: $message<br><br>Best regards,<br>The BloodConnect Team";
+                $mail->AltBody = "Dear $f_name,\n\nThank you for contacting us. We have received your message and will get back to you shortly.\n\nYour Message Summary:\nSubject: $subject\nMessage: $message\n\nBest regards,\nThe BloodConnect Team";
+
+                $mail->send();
+                $redirect_message = 'Your message has been sent successfully! A confirmation email has been sent to you.';
+            } catch (Exception $e) {
+                $redirect_message = 'Your message has been sent successfully! However, the confirmation email could not be sent.';
+            }
+
+            header("Location: landing_page.php?status=success&message=" . urlencode($redirect_message));
             exit;
         } else {
             throw new Exception('Failed to execute statement.');
